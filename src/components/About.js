d163b4a964d60c353a9e4070a7d0d721d5d325b0
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2018-04-02 14:07:57 -0700

    added to about me and test scroll func:success

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,10 @@ class About extends Component{
 					<h1>My name is Winston Summers</h1>
 					<h5>I know that was shocking to you by this time.</h5>
 				</div>
-				<p>I'm a Backend-leaning developer out of the Greater Seattle Area. Many people ask me why backend-leaning, to which I say; it's the magic of what the user doesn't see happening. UI is important, hence I prefer full stack, but its the backend where the amazing stuff happens that makes modern websites come to life; single-page apps, web-security, routing and APIs, and many more useful features that make modern web apps great.</p>
+				<p>I'm a Backend-leaning developer out of the Greater Seattle Area. Many people ask me why backend-leaning, to which I say; that's where the magic happens. UI is important, hence I prefer full stack, but its the backend where the amazing happens that makes modern websites come to life; single-page apps, web-security, routing and APIs, and many more useful features that make modern web apps great.</p>
+				<p>I'm a customer oriented developer who views scripting languages(Javascript, Python, etc) as my bread and butter. The thing that excited me the most about what I do is the constant opportunities to learn new skills and tricks, whether that's reading articles about new tech or find better solutions to the "problems" I'm working on.</p>
+				<p>Experienced leading small teams and training new hires; when a deadline needs to be met, I am frequently the one management comes to to lead the charge. Communication is one of my most valuable assets; a skill that I have refined on the job, working with clients and colleagues alike.</p>
+				<p>I am a Junior Web Developer excited to grow on existing skill sets and bring value to the team and company. Excelling at researching problems to find solutions, not giving up until the job is done. </p>
 			</div>
 		);
 	}
