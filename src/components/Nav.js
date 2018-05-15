4d3656d8bc59a500a4f02ba6a314953842b0b540
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2018-05-15 10:54:53 -0700

    fixed resume dowload bug

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ class Nav extends Component{
 			<nav>
 				<a href="#about"><strong>About</strong></a>
 				<a href="#projects"><strong>Projects</strong></a>
-				<a href="../winstonsummers.pdf" download><strong>Resume</strong></a>
+				<a download href="wsresume.pdf"><strong>Resume</strong></a>
 				<a href="https://github.com/winstonsummers" rel="noopener noreferrer" target="_blank"><strong>Github</strong></a>
 				<a href="https://www.linkedin.com/in/winston-summers/" rel="noopener noreferrer" target="_blank"><strong>Linkedin</strong></a>
 			</nav>
