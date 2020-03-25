3fb1bad994a24a307f40428a169c9488645d05fb
Patrick Hubbell <pchubbell@yahoo.com>
2020-03-25 09:48:34 -0700

    Update Resume

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ class Nav extends Component{
 			<nav>
 				<a href="#about"><strong>Patrick Hubbell</strong></a>
 				<a href="#projects"><strong>Projects</strong></a>
-				<a download href="Resume.txt"><strong>Resume</strong></a>
+				<a download href="Resume(1).txt"><strong>Resume</strong></a>
 				<a href="https://github.com/Patrick-Hubbell" rel="noopener noreferrer" target="_blank"><strong>Github</strong></a>
 				<a href="https://www.linkedin.com/in/patrickhubbell/" rel="noopener noreferrer" target="_blank"><strong>Linkedin</strong></a>
 			</nav>
