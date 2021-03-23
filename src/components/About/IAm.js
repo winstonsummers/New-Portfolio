7bc4d10512db871eb6675891e1aeb20e6e2b5384
winstonsummers <33791219+winstonsummers@users.noreply.github.com>
2021-03-23 16:01:27 -0700

    updates to about section, addressed auto scroll issues, need to update about me text

---
+++ src/components/About/IAm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Para = ({text}) => {
+    return <p>{text}</p>
+}
+
+export const Spacer = () => {
+    return <p>&nbsp;</p>
+}
+
+export default ({text}) => {
+    return <Para text={<React.Fragment>
+        <i>I am</i> {text}</React.Fragment>} />
+}
