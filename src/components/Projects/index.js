2dafe91fb281cafd6e2fecc4d3b7f505b9d9384b
winstonsummers <33791219+winstonsummers@users.noreply.github.com>
2021-03-24 10:48:49 -0700

    good for a deployment

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -10,9 +10,9 @@ export default () => {
   return (
     <div id="projects">
       
-      <Spacer />
+      {/* <Spacer />
       <h1>Here are a couple things I'm working on</h1>
-      {personalProjects}
+      {personalProjects} */}
 
       <Spacer />
       <h1>These are two of my Projects from school</h1>
